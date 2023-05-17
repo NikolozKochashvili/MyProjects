2e648b2948e4321fc822c2621238800093800d27
NikolozKochashvili <114674053+NikolozKochashvili@users.noreply.github.com>
2023-05-17 19:46:53 +0400

    Add files via upload

---
+++ Prime/Isprime/Main.py
@@ -0,0 +1,31 @@
+from math import *
+
+n = int(input('input amount of numbers: '))
+
+print("enter numbers: ")
+def check (a):
+    a = int(a)
+    pos = 2
+    while pos <= int(sqrt(a)):
+        if a % pos == 0:
+            return False
+        pos += 1
+    return True        
+
+Prime = []
+NotPrime = []
+
+for i in range(n):
+    a = int(input())
+    if check(a):    
+        Prime.append(a)
+    else:
+        NotPrime.append(a)
+
+print("prime numbers: ",end = '')
+for num in Prime:
+    print (num,end = ' ')        
+print("\n")
+print("none prime numbers: ",end='')
+for num in NotPrime:
+    print(num,end= ' ')
